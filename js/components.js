55f8fd28487079b20b42ea3a1ff84811cc6e094c
vatakudo <tannguyen1107.bku@gmail.com>
2020-02-18 19:26:41 +0700

    nop bai

--- js/components.js
+++ js/components.js
@@ -1,7 +1,34 @@
 const components = {}
 
 components.register = `
-   <div>Register Screen</div>
+<div>
+<h1>Techkids Chat</h1>
+<div class="signup">
+    <div id="signup-form">
+        <form action="">
+            <div class="input-group">
+                <label for="">Email</label>
+                <br>
+                <input type="email" placeholder="email">
+            </div>
+
+            <div class="input-group">
+                <label for="">Password</label>
+                <br>
+                <input type="password">
+            </div>
+
+            <div class="input-group">
+                <label for="">Confirm Password</label>
+                <br>
+                <input type="password">
+            </div>
+            <p>Aready have an account? Login</p>
+            <button id="button">Register</button>
+        </form>
+    </div>
+</div>
+</div>
 `
 components.login = `
 <div><h1>Login Screen</h1><div>
